3f429441edaf356a161a677b128e71cbda1cb926
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2016-12-05 17:41:29 +0100

    using range function and map function of java 8

--- src/test/java/com/algo/arrays/IslandPerimeterTest.java
+++ src/test/java/com/algo/arrays/IslandPerimeterTest.java
@@ -2,24 +2,28 @@ package com.algo.arrays;
 
 import org.testng.annotations.Test;
 
+import java.util.stream.IntStream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class IslandPerimeterTest {
 
     private int islandPerimeter(int[][] grid) {
-        int faces = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) {
-                    faces += (i - 1) < 0 ? 1 : grid[i - 1][j] == 0 ? 1 : 0;
-                    faces += (j - 1) < 0 ? 1 : grid[i][j - 1] == 0 ? 1 : 0;
-                    faces += (i + 1) >= grid.length ? 1 : grid[i + 1][j] == 0 ? 1 : 0;
-                    faces += (j + 1) >= grid[i].length ? 1 : grid[i][j + 1] == 0 ? 1 : 0;
-                }
-            }
-        }
-        return faces;
+        return IntStream.range(0, grid.length)
+                .map(i -> IntStream.range(0, grid[i].length)
+                        .map(j -> {
+                            int faces = 0;
+                            if (grid[i][j] != 0) {
+                                faces += (i - 1) < 0 ? 1 : grid[i - 1][j] == 0 ? 1 : 0;
+                                faces += (j - 1) < 0 ? 1 : grid[i][j - 1] == 0 ? 1 : 0;
+                                faces += (i + 1) >= grid.length ? 1 : grid[i + 1][j] == 0 ? 1 : 0;
+                                faces += (j + 1) >= grid[i].length ? 1 : grid[i][j + 1] == 0 ? 1 : 0;
+                            }
+                            return faces;
+                        }).sum()
+                ).sum();
+
     }
 
     @Test
