e71df52cf6ff4dca245aeb263d6f264dbf4cc691
Dheeraj Sharma <dheerajsharma1990@gmail.com>
2016-12-15 12:42:14 +0100

    change to java 8

--- src/test/java/com/algo/arrays/StringDuplicateCheckTest.java
+++ src/test/java/com/algo/arrays/StringDuplicateCheckTest.java
@@ -3,26 +3,24 @@ package com.algo.arrays;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class StringDuplicateCheckTest {
 
-    public int getDistinctWordsCount(String words) {
-        words = words.replaceAll(" +"," ").trim();
-        String[] allWords = words.split(" +");
-        Set<String> distinctWords = new HashSet<>(Arrays.asList(allWords));
-        return distinctWords.size();
+    private int getDistinctWordsCount(String words) {
+        return (int) Arrays.stream(words.split(" "))
+                .filter(s -> !s.isEmpty())
+                .distinct()
+                .count();
     }
 
     @Test
     public void shouldGetDistinctWordsCount() {
-        assertThat(getDistinctWordsCount(" ab bc  bc"), is(2));
-        assertThat(getDistinctWordsCount(" ab bc  bc  "), is(2));
-        assertThat(getDistinctWordsCount("abbc  bc  "), is(2));
+        assertThat(getDistinctWordsCount("  ab bc bc  "), is(2));
+        assertThat(getDistinctWordsCount(" ab bc   bc "), is(2));
+        assertThat(getDistinctWordsCount(" abbc bc  "), is(2));
         assertThat(getDistinctWordsCount("abbc"), is(1));
         assertThat(getDistinctWordsCount(""), is(0));
     }
